f6a3c78b160919353f4580b41c774f53fe184af1
oviebd <oviecse48@gmail.com>
2026-07-18 09:56:12 +0600

    init project

---
+++ PaginationDemo/PaginationDemo/PaginationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaginationDemoApp.swift
+//  PaginationDemo
+//
+//  Created by Habibur_Periscope on 18/7/26.
+//
+
+import SwiftUI
+
+@main
+struct PaginationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
